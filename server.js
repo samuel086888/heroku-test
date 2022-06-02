3feb6682ae5412fa8680ad503d8277a54a359cae
samuel086888 <projetdefindetude024@gmail.com>
2022-06-02 19:43:54 +0100

    ttt5

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-let express = require('express');
-let app = express();
-app.use(express.static(__dirname+'/dist/application'));
-app.get('/*',(req,resp)=>{
-    resp.sendFile(__dirname+'/dist/application/index.html');
-})
 
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/application'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/application/index.html'));});
 app.listen(process.env.PORT || 8080);
